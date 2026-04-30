063466369dddcbfd773c0a056be22f52b0632345
Jay Validzic <jmvalidzic@email.neit.edu>
2026-04-30 09:33:23 -0400

    tried doing more work

--- js/game.js
+++ js/game.js
@@ -10,23 +10,16 @@ var friction = {x:.85,y:.97}
 var stage = new GameObject({width:canvas.width, height:canvas.height});
 var level = new GameObject({x:0,y:0});
 
-var ground = new GameObject({width:canvas.width*10, x:canvas.width*10/2-200,height:64,y:canvas.height-32, color:"green", world:level})
+var ground = new GameObject({width:canvas.width*10, x:canvas.width*10/2-200,height:64,y:canvas.height-32, world:level})
 ground.img.src=`images/ground.png`
 
-var sky = new GameObject({width:canvas.width, height:canvas.height})
-sky.img.src = `images/skyBackground.png`
-
-var g1 = new Group();
-g1.color= `rgb(251,0,254)`;
-g1.add([ground]);
-
-var rects = new Group();
-rects.add([ground]);
+var bg = new GameObject({x:level.x,y:level.y, width:canvas.width*4, height:canvas.height})
+bg.img.src = `images/skyBackground.png`
 
 function animate()
 {
     context.clearRect(0,0,canvas.width, canvas.height);
     
-    sky.drawStaticImage();
-    ground.drawStaticImage();
+    bg.drawStaticImage({x:0,y:0});
+    ground.drawStaticImage({x: 0, y: 100});
 }
